da4e069acb4a31c04e389d63d32b411aedeaf374
huzibo-yyds <2487893763@qq.com>
2025-11-24 11:00:55 +0800

    437 update 前缀和prefix & 哈希表实现

--- hot100/binary_tree/437_pathSum.py
+++ hot100/binary_tree/437_pathSum.py
@@ -55,9 +55,48 @@ class Solution:
         '''
         2- 前缀和 + 哈希表
             核心思路
+            将求“ 两点间路径和问题 ” -> “求两个前缀和的查的问题”
+            问题定义：prefix(C) - prefix(P) = targetSum （C为当前节点，P为祖先节点）
+            当我们遍历到节点 C 时，在从根到 C 的路径上，我们只需要回头找一找，之前出现过多少个前缀和恰好等于 prefix(C) - targetSum
+
             - 定义从当前节点到根节点的前缀和sum(curr)
             - 若存在某个在当前路径上的先前前缀和 prev，使得 sum(curr) - prev == targetSum，
                 那么从 prev 的下一个节点到当前节点的这段路径和为 targetSum。
             - 因此需要一个哈希表 count，其中 count[s] 表示在当前递归路径上出现前缀和为 s 的次数
+
+            ‘哈希表’ 在python中为dict
+            - 用于高效完成回头找，前缀节点
+
         '''
-        pass
+        if root is None:
+            return 0
+        
+        prefix_counts = {0:1}
+        ans = 0 # 结果计数
+
+        def dfs(node:TreeNode, curr_sum:int):
+            nonlocal ans
+            if node is None:
+                return
+            
+            curr_sum+=node.val
+
+            # 📍关键计算 prefix(P)  = prefix(C) - targetSum
+            ans+= prefix_counts.get(curr_sum-targetSum,0) # Return the value for key if key is in the dictionary, else default.
+
+            # 将当前前缀和加入哈希表
+            prefix_counts[curr_sum] = prefix_counts.get(curr_sum,0) + 1
+
+            dfs(node.left,curr_sum)
+            dfs(node.right,curr_sum)
+
+            # 回溯：离开当前节点时撤销当前前缀和的计数，确保哈希表只反映当前路径
+            prefix_counts[curr_sum]-=1
+
+            #
+            if prefix_counts[curr_sum] == 0:
+                del prefix_counts[curr_sum]
+        
+        dfs(root,0)
+        return ans
+        
